17a7e17d4d8deaa9d87fcf1a83e351da027d30d9
Rubén Valls Aparici <r.vallsaparici@gmail.com>
2024-03-07 17:24:17 +0100

    Add routes for RecetasPageComponent and PlanificacionPageComponent

---
+++ src/app/pages/planificacion/planificacion.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlanificacionPageComponent } from './page/planificacion-page/planificacion-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    PlanificacionPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PlanificacionModule { }
