37b0352c413a989f1c88e594763913d2ac526457
Rubén Valls Aparici <r.vallsaparici@gmail.com>
2024-03-11 10:25:54 +0100

    Add meal options to nutrition week meals component

--- src/app/pages/planificacion/components/nutrition-week-meals/nutrition-week-meals.component.ts
+++ src/app/pages/planificacion/components/nutrition-week-meals/nutrition-week-meals.component.ts
@@ -1,13 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DIAS, MESES } from '../../../../constants/dates.constants';
+import { MEALS } from '../../../../constants/meals.constants';
 
 @Component({
   selector: 'app-nutrition-week-meals',
   templateUrl: './nutrition-week-meals.component.html',
   styleUrl: './nutrition-week-meals.component.scss'
 })
-export class NutritionWeekMealsComponent {
+export class NutritionWeekMealsComponent implements OnInit{
   @Input({required: true}) day!: Date;
   spanishDays = DIAS;
   spanishMonths = MESES;
+  mealsButtons: string[] = [];
+
+  ngOnInit() {
+    Object.values(MEALS).forEach((meal) => {
+      this.mealsButtons.push(meal);
+    });
+  }
 }
